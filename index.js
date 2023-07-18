fe9fefc4787cd94d9f5efc38f87b41eace5d1ada
nikkivno <nikkivigneault@gmail.com>
2023-07-18 19:52:12 -0300

    Start of .env file setup to protect database info

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 const inquirer = require('inquirer');
+const mysql = require('mysql2');
+require('dotenv').config();
 
+const connection = mysql.createConnection({
+    process.env.DB_Host,
+    process.env.DB_Username,
+    process.env.DB_Password,
+    process.env.DB_Name
+});
+
+connection.connect();
+ 
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results[0].solution);
+  });
+   
+  connection.end();
 
 inquirer.prompt ([
     {
-        
+        type: "rawlist",
+        message:"Please choose one of the following:",
+        choices: [
+            'View All Departments',
+            'View All Roles',
+            'View All Employees',
+            'Add a Department',
+            'Add a Role',
+            'Add an Employee',
+            'Update an Employee Role'
+        ],
+        name: "Options"
     }
 ])
