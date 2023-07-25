153bf86bd2a0bf1f04155eb2d81a662474f2771a
nikkivno <nikkivigneault@gmail.com>
2023-07-25 01:38:29 -0300

    working on viewRoles functoin to add department name

--- index.js
+++ index.js
@@ -98,25 +98,36 @@ function viewDepartments() {
 }
 
 function viewRoles() {
-    connection.query('SELECT * FROM `role`', function (err, results){
-        if (err) throw err;
-        console.table(results);
-        options();
-    });
-}
 
-function viewEmployees() {
     const sqlQuery = 
-    `SELECT employee.id, first_name,last_name, title, manager_id FROM employee
-    LEFT JOIN role
-    ON employee.role_id = role.id`;
+    `SELECT role.id, title, department.name AS department, salary
+    FROM role
+    LEFT JOIN department
+    ON role.department_id = department.name`;
 
     connection.query(sqlQuery, (err, results) => {
         if (err) throw err;
         console.table(results);
         options();
         });
-      };
+      }
+
+
+
+function viewEmployees() {
+    const sqlQuery = 
+    `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, salary,
+    CONCAT(manager.first_name,' ', manager.last_name) AS manager FROM employee
+    LEFT JOIN role ON employee.role_id = role.id
+    LEFT JOIN department ON role.department_id = department.id
+    LEFT JOIN employee AS manager ON employee.manager_id = manager.id`;
+
+    connection.query(sqlQuery, (err, results) => {
+        if (err) throw err;
+        console.table(results);
+        options();
+        });
+      }
 
 
 
